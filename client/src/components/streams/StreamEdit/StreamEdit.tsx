023d024f2d3534c6af83883025a24a4d25967232
User <yonis676@gmail.com>
2020-11-22 11:08:42 +0200

    getting data for edit stream

--- client/src/components/streams/StreamEdit/StreamEdit.tsx
+++ client/src/components/streams/StreamEdit/StreamEdit.tsx
@@ -1,15 +1,42 @@
 import * as React from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { connect } from 'react-redux';
+import { Stream } from './../../../models/stream';
+import { fetchStream } from './../../../actions/index';
+
 
 export interface Props {
-    children?: React.ReactNode
+    children?: React.ReactNode,
+    stream: Stream;
+    fetchStream: Function;
 }
 
 export interface State {
 }
 
-const StreamEdit = (props: Props, state: State) => (
-    <div>StreamEdit</div>
-)
+export interface RouteParams {
+    id: string;
+}
+class StreamEdit extends React.Component<RouteComponentProps<RouteParams> & Props, State> {
+    componentDidMount(): void {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        else return (
+            <div>{this.props.stream.title}</div>
+        )
+    }
+}
 
 
-export default StreamEdit;
+const mapStateToProps = (state: any, ownProps: RouteComponentProps<RouteParams> & Props) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+};
+
+
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
--- client/src/components/streams/StreamList/StreamList.tsx
+++ client/src/components/streams/StreamList/StreamList.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom';
+import history from '../../../history';
 import { fetchStreams } from './../../../actions/index';
 import { Stream } from './../../../models/stream';
 
@@ -23,7 +24,7 @@ export class StreamList extends Component<Props, State> {
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">Edit</button>
+                    <Link className="ui button primary" to={`streams/edit/${stream.id}`}>Edit</Link>
                     <button className="ui button negative">Delete</button>
                 </div>
             );
